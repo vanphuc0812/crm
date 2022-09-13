32a95e73029cca323cccb1964e1cf3b1a5e9e5cf
vanphuc0812 <vanphuc0812@gmail.com>
2022-09-13 21:46:35 +0700

    Add custom front-end source

---
+++ LayoutCrm/custom/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    // Apply alt class on alternate table row
+    // $("table tbody tr:nth-child(2n)").addClass("alt");
+
+
+    $("table tbody tr:nth-child(2n)").addClass('highlight-add');
+    setTimeout(function () {
+        $("table tbody tr:nth-child(2n)").removeClass('highlight-add');
+    }, 2000);
+    
+});
